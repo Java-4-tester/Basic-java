dfcbff5188aa288ddeacaef0777206d440f3f4fa
NadeemQA <nadeem.ahamad.7291@gmail.com>
2023-10-15 17:17:27 +0530

    Commited the code

---
+++ BasicOne.java
@@ -0,0 +1,24 @@
+public class BasicOne {
+
+    public static void main (String... args){
+
+        System.out.println("This is Basic of java - included - Arguments, Data and Array");
+
+        printMyName();
+        printTopicsName();
+    }
+    public static void printMyName(){
+        System.out.println("Nadeem");
+        printTheCourse();
+    }
+    public static void printTheCourse(){
+
+        System.out.println("The Course:- " +"Java for Testers");
+    }
+    public static void printTopicsName(){
+
+        System.out.println("Topics:-" +"Arguments, Array, int etc");
+
+    }
+
+}
