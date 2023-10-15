0f0859eb8e5d2db48066ae5b29e1026c61057f4e
NadeemQA <nadeem.ahamad.7291@gmail.com>
2023-10-15 16:43:06 +0530

    additional information shared

--- ArrayExample.java
+++ ArrayExample.java
@@ -5,8 +5,14 @@ public class ArrayExample {
         String[] arrayOfName = {"A", "B", "C", "D"};
         int[] number = {12, 2, 8, 3};
 
+        /*arrayOfName is an array of strings, and number is an array of integers. Arrays are used to store multiple values of the same data type. In this case, arrayOfName stores four strings, and number stores four integers. */
+
         System.out.println(arrayOfName[3]);
         System.out.println(number[0] + number[3]);
+
+        /*arrayOfName[3] accesses the fourth element (index 3) of the arrayOfName array, which is "D." In Java, array indices start from 0, so the first element is at index 0, the second at index 1, and so on.
+
+number[0] + number[3] adds the values of the first element (index 0) and the fourth element (index 3) of the number array. In this case, it adds 12 and 3, resulting in the value 15. */
     }
     
 }
